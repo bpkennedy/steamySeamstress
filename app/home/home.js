b6d343ef7a72b1c112e144ddf0f023c53e164cc9
Brian Kennedy <bpkennedy@gmail.com>
2015-05-17 22:09:49 -0500

    Done more work, son.

--- app/home/home.js
+++ app/home/home.js
@@ -23,4 +23,19 @@ angular.module('myApp.home', ['ngRoute', 'ngAnimate', 'firebase'])
         $timeout(function() {
           $scope.allPortfolio = $firebase(ref2).$asArray();
         }, 50);
+
+        $(window).bind('scroll',function(e){
+        	parallaxScroll();
+        });
+
+        function parallaxScroll(){
+        	var scrolled = $(window).scrollTop();
+          var theta = $(window).scrollTop() % Math.PI;
+          $('#leftGear').css({ transform: 'rotate(' + theta + 'rad)' });
+          $('#rightGear').css({ transform: 'rotate(-' + theta + 'rad)' });
+
+        	$('#backgroundLayer').css('top',(0-(scrolled*.85))+'px');
+        	$('#middleLayer').css('top',(0-(scrolled*.5))+'px');
+        	$('#frontLayer').css('top',(0-(scrolled*.15))+'px');
+        }
 });
