46fb15dacc1ac412c580eddc98cf62b582d92065
Brian Kennedy <bpkennedy@gmail.com>
2015-05-18 22:50:17 -0500

    added contact-us.php file and PHPMailer folder

--- app/home/home.js
+++ app/home/home.js
@@ -10,7 +10,7 @@ angular.module('myApp.home', ['ngRoute', 'ngAnimate', 'firebase'])
   });
 }])
 
-.controller('HomeCtrl', function($scope, $timeout, $firebase, $rootScope) {
+.controller('HomeCtrl', function($scope, $timeout, $firebase, $rootScope, $http) {
         var ref = new Firebase("https://steamy.firebaseio.com/services");
         var ref2 = new Firebase("https://steamy.firebaseio.com/portfolio");
         $scope.allServices = [];
@@ -37,5 +37,39 @@ angular.module('myApp.home', ['ngRoute', 'ngAnimate', 'firebase'])
         	$('#backgroundLayer').css('top',(0-(scrolled*.85))+'px');
         	$('#middleLayer').css('top',(0-(scrolled*.5))+'px');
         	$('#frontLayer').css('top',(0-(scrolled*.15))+'px');
+        }
+
+        //contact form
+        $scope.result = 'hidden'
+        $scope.resultMessage;
+        $scope.formData; //formData is an object holding the name, email, subject, and message
+        $scope.submitButtonDisabled = false;
+        $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
+        $scope.submit = function(contactform) {
+           $scope.submitted = true;
+           $scope.submitButtonDisabled = true;
+           if (contactform.$valid) {
+               $http({
+                   method  : 'POST',
+                   url     : 'contact-form.php',
+                   data    : $.param($scope.formData),  //param method from jQuery
+                   headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+               }).success(function(data){
+                   console.log(data);
+                   if (data.success) { //success comes from the return json object
+                       $scope.submitButtonDisabled = true;
+                       $scope.resultMessage = data.message;
+                       $scope.result='bg-success';
+                   } else {
+                       $scope.submitButtonDisabled = false;
+                       $scope.resultMessage = data.message;
+                       $scope.result='bg-danger';
+                   }
+               });
+           } else {
+               $scope.submitButtonDisabled = false;
+               $scope.resultMessage = 'Failed <i class="fa fa-frown"></i>  Please fill out all the fields.';
+               $scope.result='bg-danger';
+           }
         }
 });
